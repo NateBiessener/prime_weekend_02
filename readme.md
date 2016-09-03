44da5878056f38b433bf643b49fba766a8c37894
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-03 14:42:09 -0500

    display and functional buttons

--- readme.md
+++ readme.md
@@ -5,11 +5,12 @@ Plan
 ____
 
 * 0.1 X sources
-* 0.2 confirm ajax call works
-* 0.3 add html display areas
-* 0.4 add prev/next buttons
-* 0.5 add people buttons
-* 0.6 add transition animation
-* 0.7 add timer
-* 0.8 reset timer on any button click
+* 0.2 X confirm ajax call works
+* 0.3 X add html display areas
+* 0.4 X add prev/next buttons
+* 0.5 X make the display and buttons work
+* 0.6 add people buttons
+* 0.7 add transition animation
+* 0.8 add timer
+* 0.9 reset timer on any button click
 * 1.0 cleanup and push
--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,44 @@
 console.log('script sourced');
 
-$(document).ready(function(){
-  console.log('jq works');
-})
-
+var studentData = [];
 var url = 'http://devjana.net/pi/pi_students.json';
 
-$.ajax({
-  url: url,
-  dataType: 'JSON',
-  success: function(data){
-    console.log('in sucess');
-    console.log(data);
-    for (var i = 0; i < data.students.length; i++) {
-      console.log(data.students[i]);
-    }
-  }
+$(document).ready(function(){
+  console.log('jq works');
+  //pulls students JSON file and puts into studentData, puts first student on DOM
+  $.ajax({
+    url: url,
+    dataType: 'JSON',
+    success: function(data){
+      console.log('in sucess');
+      // console.log(data);
+      studentData = data.students;
+      console.log(studentData);
+      $('#outDiv').html('<p>' + studentData[0].first_name + " " + studentData[0].last_name + '</p><p>' + studentData[0].info + '</p><p class="current" id="' + 0 + '">' + 1 + "/" + studentData.length + '</p>');
+    }//end success callback
+  });//end ajax call
+})//end doc ready
 
-});
+var lastPerson = function(){
+  var index = Number($('.current').attr('id'));
+  if (index === 0) {
+    $('#outDiv').html('<p>' + studentData[studentData.length - 1].first_name + " " + studentData[studentData.length - 1].last_name + '</p><p>' + studentData[studentData.length - 1].info + '</p><p class="current" id="' + (studentData.length - 1) + '">' + studentData.length + "/" + studentData.length + '</p>');
+  }//end if
+  else {
+    $('#outDiv').html('<p>' + studentData[index - 1].first_name + " " + studentData[index - 1].last_name + '</p><p>' + studentData[index - 1].info + '</p><p class="current" id="' + (index - 1) + '">' + index + "/" + studentData.length + '</p>');
+  }//end else
+};//end lastPerson
+
+var nextPerson = function(){
+  var index = Number($('.current').attr('id'));
+  if (index === studentData.length - 1) {
+    $('#outDiv').html('<p>' + studentData[0].first_name + " " + studentData[0].last_name + '</p><p>' + studentData[0].info + '</p><p class="current" id="' + 0 + '">' + 1 + "/" + studentData.length + '</p>');
+  }//end if
+  else {
+    $('#outDiv').html('<p>' + studentData[index + 1].first_name + " " + studentData[index + 1].last_name + '</p><p>' + studentData[index + 1].info + '</p><p class="current" id="' + (index + 1) + '">' + (index+2) + "/" + studentData.length + '</p>');
+  }//end else
+};//end nextPerson
+
+// first_name:"Will"
+// info:"aka Mr. 5/4"
+// last_name:"Cruzen"
