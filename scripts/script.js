9876917942fa0de82fd30d6aafc63e23d86dba7e
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-05 14:23:40 -0500

    remove global array for JSON data

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 console.log('script sourced');
 
-var studentData = [];
+// var studentData = [];
 var url = 'http://devjana.net/pi/pi_students.json';
 var counter = 9;
 
@@ -13,14 +13,14 @@ $(document).ready(function(){
     success: function(data){
       // console.log('in success');
       // console.log(data);
-      studentData = data.students;
+      // studentData = data.students;
       // console.log(studentData);
-      $('#outDiv').html('<p>' + studentData[0].first_name + ' ' + studentData[0].last_name + '</p><p>' + studentData[0].info + '</p><p>' + 1 + '/' + studentData.length + '</p>');
+      $('#outDiv').html('<p>' + data.students[0].first_name + ' ' + data.students[0].last_name + '</p><p>' + data.students[0].info + '</p><p>' + 1 + '/' + data.students.length + '</p>');
       $('#outDiv').data('position', 0);
       $('#nextBtn').remove();
-      for (var i = 0; i < studentData.length; i++) {
-        var studentButton = $('<button class="studentBtn" id="' + i + '">' + studentData[i].first_name + '</button>');
-        studentButton.data('student', studentData[i]);
+      for (var i = 0; i < data.students.length; i++) {
+        var studentButton = $('<button class="studentBtn" id="' + i + '">' + data.students[i].first_name + '</button>');
+        studentButton.data('student', data.students[i]);
         $('#buttons').append(studentButton);
       }
       $('#buttons').append('<button id="nextBtn" onclick="nextStudent()">Next</button>');
@@ -39,7 +39,7 @@ $(document).ready(function(){
         counter = 9;
         var student = $(this).data('student');
         var id = $(this).attr('id');
-        var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (Number(id) + 1) + '/' + studentData.length + '</p>';
+        var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (Number(id) + 1) + '/' + $('.studentBtn').length + '</p>';
         $('#outDiv').fadeOut(400, function(){
           $('#outDiv').html(newDisp);
           $('#outDiv').data('position', id);
@@ -57,17 +57,17 @@ var prevStudent = function(){
   counter = 9;
   var index = Number($('#outDiv').data('position'));
   if (index === 0) {
-    var student = $('#' + (studentData.length - 1)).data('student');
-    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (studentData.length) + '/' + studentData.length + '</p>';
+    var student = $('#' + ($('.studentBtn').length - 1)).data('student');
+    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + ($('.studentBtn').length) + '/' + $('.studentBtn').length + '</p>';
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
-      $('#outDiv').data('position', (studentData.length - 1));
+      $('#outDiv').data('position', ($('.studentBtn').length - 1));
       $('#outDiv').fadeIn(400);
     });
   }//end if
   else {
     var student = $('#' + (index - 1)).data('student');
-    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + index + '/' + studentData.length + '</p>';
+    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + index + '/' + $('.studentBtn').length + '</p>';
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', (index - 1));
@@ -79,9 +79,9 @@ var prevStudent = function(){
 var nextStudent = function(){
   counter = 9;
   var index = Number($('#outDiv').data('position'));
-  if (index === studentData.length - 1) {
+  if (index === $('.studentBtn').length - 1) {
     var student = $('#' + 0).data('student');
-    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>1/' + studentData.length + '</p>';
+    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>1/' + $('.studentBtn').length + '</p>';
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', 0);
@@ -90,7 +90,7 @@ var nextStudent = function(){
   }//end if
   else {
     var student = $('#' + (index + 1)).data('student');
-    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (index + 2) + '/' + studentData.length + '</p>';
+    var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (index + 2) + '/' + $('.studentBtn').length + '</p>';
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', index + 1);
