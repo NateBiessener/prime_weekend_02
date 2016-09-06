0ab2de8ff2fc5b1d54d2e9c98ae76f9e7bb45422
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-05 23:43:23 -0500

    cleanup odd & ends

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,26 @@ var counter = 9;
 
 $(document).ready(function(){
   console.log('jq works');
-  //pulls students JSON file and puts into studentData, puts first student on DOM
   $.ajax({
     url: url,
     dataType: 'JSON',
     success: function(data){
       // console.log('in success');
       // console.log(data);
-      // studentData = data.students;
-      // console.log(studentData);
+      //display first student and add it's position to #outDiv's data attr
       $('#outDiv').html('<p>' + data.students[0].first_name + ' ' + data.students[0].last_name + '</p><p>' + data.students[0].info + '</p><p>' + 1 + '/' + data.students.length + '</p>');
       $('#outDiv').data('position', 0);
+      //remove 'next' button to be added back after all other buttons
       $('#nextBtn').remove();
+      //add button for each student, and store object data within each button
       for (var i = 0; i < data.students.length; i++) {
         var studentButton = $('<button class="studentBtn" id="' + i + '">' + data.students[i].first_name + '</button>');
         studentButton.data('student', data.students[i]);
         $('#buttons').append(studentButton);
       }
+      //add 'next' button back to DOM
       $('#buttons').append('<button id="nextBtn" onclick="nextStudent()">Next</button>');
+      //calls nextStudent every 10 seconds, unless a button was clicked - counter gets reset
       setInterval(function(){
         if (counter == 0) {
           // console.log(counter);
@@ -37,9 +39,12 @@ $(document).ready(function(){
       //start onclick for individual buttons
       $( 'body' ).on( 'click', '.studentBtn', function(){
         counter = 9;
+        //variables for readability
         var student = $(this).data('student');
         var id = $(this).attr('id');
+        //build new display html
         var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (Number(id) + 1) + '/' + $('.studentBtn').length + '</p>';
+        //fadeOut #outDiv, change its html and data, then fadeIn
         $('#outDiv').fadeOut(400, function(){
           $('#outDiv').html(newDisp);
           $('#outDiv').data('position', id);
@@ -56,9 +61,11 @@ $(document).ready(function(){
 var prevStudent = function(){
   counter = 9;
   var index = Number($('#outDiv').data('position'));
+  //if at start, use last student
   if (index === 0) {
     var student = $('#' + ($('.studentBtn').length - 1)).data('student');
     var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + ($('.studentBtn').length) + '/' + $('.studentBtn').length + '</p>';
+    //fadeOut, update #outDiv, fadeIn
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', ($('.studentBtn').length - 1));
@@ -66,8 +73,10 @@ var prevStudent = function(){
     });
   }//end if
   else {
+    //use previous student
     var student = $('#' + (index - 1)).data('student');
     var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + index + '/' + $('.studentBtn').length + '</p>';
+    //fadeOut, update #outDiv, fadeIn
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', (index - 1));
@@ -79,9 +88,11 @@ var prevStudent = function(){
 var nextStudent = function(){
   counter = 9;
   var index = Number($('#outDiv').data('position'));
+  //if at end of students, use first student
   if (index === $('.studentBtn').length - 1) {
     var student = $('#' + 0).data('student');
     var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>1/' + $('.studentBtn').length + '</p>';
+    //fadeOut, update #outDiv, fadeIn
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', 0);
@@ -89,8 +100,10 @@ var nextStudent = function(){
     });
   }//end if
   else {
+    //use next student
     var student = $('#' + (index + 1)).data('student');
     var newDisp = '<p>' + student.first_name + ' ' + student.last_name + '</p><p>' + student.info + '</p><p>' + (index + 2) + '/' + $('.studentBtn').length + '</p>';
+    //fadeOut, update #outDiv, fadeIn
     $('#outDiv').fadeOut(400, function(){
       $('#outDiv').html(newDisp);
       $('#outDiv').data('position', index + 1);
